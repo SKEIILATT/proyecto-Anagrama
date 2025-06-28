efbc5e4cf9c1a7d3a845c947070799c518356be7
SKEIILATT <javimacias619@gmail.com>
2025-06-28 18:06:44 -0500

    Se realizó la configuración de no case sensitive para los carácteres ingresados por el usuario

--- anagrama.asm
+++ anagrama.asm
@@ -52,10 +52,115 @@ main proc
     ;Inicializo y cargo en memoria el puntero para el segundo buffer
     lea dx, buffer_e2
     mov ah, 0Ah
-    int 21h 
+    int 21h  
+    ;Salto de linea
+    mov ah, 02h  ; Instruccion de escritura de un caracter
+    mov dl, 0Dh  ; Retorno de carro para volver al inicio del prompt
+    int 21h      ; Muestro en pantalla
+    
+    mov ah, 02h  ; Instruccion de escritura de un caracter
+    mov dl, 0Ah  ; Instruccion de avance de linea
+    int 21h      ; Muestro en pantalla
     
     ;Punteros en el primer caracter de cada buffer para comenzar con el programa
     lea si, buffer_e1+2
+    lea di, buffer_e2+2 
+ 
+    ;Iniciar contador para convertir en minuscula todas las letras
+    mov cl, [buffer_e1+1] 
+    mov ch, 0
+    
+verificar_letra_b1:   
+    ;Movemos el puntero al primer caracter del buffer
+    mov al, [si]                                     
+    ;Comparo el valor ASCII de ambos caracteres
+    ;Para saber si es mayuscula, el valor ASCII debe de estar entre 65 y 90 
+    cmp al, 'A' ; 'A' representa el 65 en ASCII 
+    ;Si el codigo ASCII de al es menor que 65, salta a no_es_mayus debido a que ya sabemos que no sera mayuscula
+    jl no_es_mayus_b1
+    cmp al, 'Z'; 'Z' representa el 90 en ASCII
+    ;Si es mayor que 90, saltamos de igual manera a no_es_mayus debido a que ya sabemos que no es mayuscula
+    jg no_es_mayus_b1
+    ;Si es mayuscula, la transformamos a minuscula
+    add al, 'a'- 'A' ; Para transformar a minuscula, basta con restar la diferencia entre el valor ASCII de la minuscula con la mayuscula
+    ;Reescribimos en el buffer el nuevo valor
+    mov [si], al
+
+;Etiqueta que dado que no es mayuscula, avanza al siguiente caracter y repite el bucle
+no_es_mayus_b1:
+    inc si
+    loop verificar_letra_b1    
+    
+    mov cl, [buffer_e2+1]
+    mov ch, 0
+
+verificar_letra_b2:   
+    ;Movemos el puntero al primer caracter del buffer
+    mov al, [di]                                     
+    ;Comparo el valor ASCII de ambos caracteres
+    ;Para saber si es mayuscula, el valor ASCII debe de estar entre 65 y 90 
+    cmp al, 'A' ; 'A' representa el 65 en ASCII 
+    ;Si el codigo ASCII de al es menor que 65, salta a no_es_mayus debido a que ya sabemos que no sera mayuscula
+    jl no_es_mayus_b2
+    cmp al, 'Z'; 'Z' representa el 90 en ASCII
+    ;Si es mayor que 90, saltamos de igual manera a no_es_mayus debido a que ya sabemos que no es mayuscula
+    jg no_es_mayus_b2
+    ;Si es mayuscula, la transformamos a minuscula
+    add al, 'a'- 'A' ; Para transformar a minuscula, basta con restar la diferencia entre el valor ASCII de la minuscula con la mayuscula
+    ;Reescribimos en el buffer el nuevo valor
+    mov [di], al
+
+;Etiqueta que dado que no es mayuscula, avanza al siguiente caracter y repite el bucle
+no_es_mayus_b2:
+    inc di
+    loop verificar_letra_b2
+           
+    ;Esta seccion solo la puse para probar que vale xd lo de no case sensitive"
+    ;Esto despues se borrar y se sigue con la logica
+    ;Mostrar cadena
+    lea si, buffer_e1+2
+    mov cl, [buffer_e1+1]
+    mov ch, 0
+    add si, cx
+    mov byte ptr [si], '$'
+    lea dx, buffer_e1+2
+    mov ah, 09h
+    int 21h 
+    ;Salto de linea
+    mov ah, 02h  ; Instruccion de escritura de un caracter
+    mov dl, 0Dh  ; Retorno de carro para volver al inicio del prompt
+    int 21h      ; Muestro en pantalla
+        
+    mov ah, 02h  ; Instruccion de escritura de un caracter
+    mov dl, 0Ah  ; Instruccion de avance de linea
+    int 21h      ; Muestro en pantalla
+        
+    ;Mostrar cadena
     lea di, buffer_e2+2
+    mov cl, [buffer_e2+1]
+    mov ch, 0
+    add di, cx
+    mov byte ptr [di], '$'
+    lea dx, buffer_e2+2
+    mov ah, 09h
+    int 21h          
+    
+    ;Terminar programa
+    mov ah, 4Ch
+    int 21h
+    
+    main endp
+    end main
     
     
+    
+    
+        
+    
+ 
+    
+    
+    
+    
+    
+                      
